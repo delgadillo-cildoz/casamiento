626b426b5a90a6cc16241055c28ae5d5b25ba0ee
FranciscoRomero01 <89942141+FranciscoRomero01@users.noreply.github.com>
2024-02-21 16:33:34 -0300

    Add files via upload

---
+++ reloj.js
@@ -0,0 +1,49 @@
+simplyCountdown('#cuenta', {
+	year: 2024, // required
+	month: 5, // required
+	day: 4, // required
+	hours: 15, // Default is 0 [0-23] integer
+	minutes: 30, // Default is 0 [0-59] integer
+	seconds: 0, // Default is 0 [0-59] integer
+	words: { //words displayed into the countdown
+		days: 'Día',
+		hours: 'Hora',
+		minutes: 'Minuto',
+		seconds: 'Segundo',
+		pluralLetter: 's'
+	},
+	plural: true, //use plurals
+	inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
+	inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
+	// in case of inline set to false
+	enableUtc: true, //Use UTC as default
+	refresh: 1000, // default refresh every 1s
+	sectionClass: 'simply-section', //section css class
+	amountClass: 'simply-amount', // amount css class
+	wordClass: 'simply-word', // word css class
+	zeroPad: false,
+	countUp: false
+});
+
+
+var instance = M.Carousel.getInstance(elem);
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+});
+
+// Or with jQuery
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+
+
+  /* jQuery Method Calls
+    You can still use the old jQuery plugin method calls.
+    But you won't be able to access instance properties.
+
+    $('.carousel').carousel('methodName');
+    $('.carousel').carousel('methodName', paramName);
+  */
